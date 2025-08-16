51c1063ec2acef9c0b8519bbbe1c21de7c1e2b80
Anushka Nagdeote <125575070+anu4619@users.noreply.github.com>
2025-08-17 00:15:56 +0530

    Time: 468 ms (67.61%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select today.id
+from weather today
+join weather yesterday
+on datediff(today.recordDate,yesterday.recorddate)=1
+where today.temperature>yesterday.temperature
