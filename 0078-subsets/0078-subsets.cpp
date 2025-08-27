efe832fa90e87cebe8e8922c162c59528f102515
Anushka Nagdeote <125575070+anu4619@users.noreply.github.com>
2025-08-27 23:28:29 +0530

    Time: 1 ms (46.69%), Space: 12.8 MB (25.54%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    private:
+    void solve(vector<int>& output,vector<vector<int>>& ans,int index,vector<int>& nums){
+        //base case
+        if(index>=nums.size())
+        {
+            ans.push_back(output);
+            return;
+        }
+        //exclude
+        solve(output,ans,index+1,nums);
+
+        // include
+        int e=nums[index];
+        output.push_back(e);
+        solve(output,ans,index+1,nums);
+
+        output.pop_back();
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> output;
+        vector<vector<int>> ans;
+        int index=0;
+        solve(output,ans,index,nums);
+      return ans;
+    }
+};
