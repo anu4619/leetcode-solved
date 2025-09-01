4431385f5e88d3b0ffd4849b4dc236fbcf06be17
Anushka Nagdeote <125575070+anu4619@users.noreply.github.com>
2025-09-01 19:47:53 +0530

    Time: 945 ms (47.24%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -8,4 +8,4 @@ left join examinations e
 on s.student_id=e.student_id
 and sub.subject_name=e.subject_name
 group by s.student_id,s.student_name,sub.subject_name
-order by s.student_id,s.student_name,sub.subject_name
+order by s.student_id,s.student_name
