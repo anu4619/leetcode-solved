0a41782bb3008e0f6ece9698e6a79afa37515662
Anushka Nagdeote <125575070+anu4619@users.noreply.github.com>
2025-09-03 00:54:51 +0530

    Time: 13 ms (45.19%), Space: 32.6 MB (74.81%) - LeetHub

--- 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& points) {
-        sort(points.begin(), points.end(), [](const auto& a, const auto& b) {
-            if (a[0] == b[0]) return a[1] > b[1]; // y descending if x equal
-            return a[0] < b[0];                   // x ascending
-        });
+        int count=0;
+    for(int i=0;i<points.size();i++)
+    {
+        int ax=points[i][0];
+        int ay=points[i][1];
+        for(int j=0;j<points.size();j++){
+            if (i == j) continue;
+        int bx=points[j][0];
+        int by=points[j][1];
+        bool isUpperLeft = (ax < bx && ay > by);
+                bool isHorizontalLine = (ay == by && ax < bx);
+                bool isVerticalLine   = (ax == bx && ay > by);
+        if(isUpperLeft || isHorizontalLine || isVerticalLine){
+            bool valid= true;
 
-        int n = points.size(), result = 0;
-        for (int i = 0; i < n; i++) {
-            int top = points[i][1];
-            int bot = INT_MIN;
-            for (int j = i + 1; j < n; j++) {
-                int y = points[j][1];
-                if (bot < y && y <= top) {
-                    result++;
-                    bot = y;
-                    if (bot == top) break;
-                }
+        
+        for(int k=0;k<points.size();k++)
+        {
+            if(i==k||j==k) continue;
+            int kx=points[k][0],ky=points[k][1];
+            if(ax<=kx && kx<=bx && by<=ky && ky<=ay){
+                valid=false;break;
             }
-        }
-        return result;
+            
+            }
+            if (valid){
+                count++;
+        
+    }}}}
+    return count;
+        
+        
     }
 };
