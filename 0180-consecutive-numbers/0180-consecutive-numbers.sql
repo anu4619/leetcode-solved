0b8389452beaa5ee1bbfd5f20637845a76bf00af
Anushka Nagdeote <125575070+anu4619@users.noreply.github.com>
2025-12-31 00:55:41 +0530

    Time: 484 ms (88.51%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,10 +1,6 @@
 # Write your MySQL query statement below
-with cte as (
-    select num,
-    lag(num) over(order by id) as previous,
-    lead(num) over(order by id) as post
-    from logs
-)
-
-select distinct num as ConsecutiveNums
-from cte where num=previous and num=post
+select distinct l.num as ConsecutiveNums from logs l
+inner join logs prev
+on l.num=prev.num and l.id-prev.id=1
+inner join logs next
+on l.num=next.num and next.id-l.id=1
